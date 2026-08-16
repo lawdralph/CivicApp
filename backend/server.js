918dac1ca73ae71758a3131adcbfe4ae2dcbc457
lawdralph <rafiuazeez01@gmail.com>
2026-08-16 20:07:11 +0100

    submission error fixed

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,21 @@ const app = express();
 const port = config.port;
 let memoryServer = null;
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  res.setHeader('Access-Control-Allow-Origin', origin || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
